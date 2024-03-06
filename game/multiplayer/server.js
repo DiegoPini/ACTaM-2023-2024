4ad48ee1f8a881c94d4c6485d02bec8b27cbba90
DiegoPini <100426280+DiegoPini@users.noreply.github.com>
2024-03-06 16:53:40 +0100

    game changes

--- game/multiplayer/server.js
+++ game/multiplayer/server.js
@@ -57,7 +57,7 @@ const map = new mapboxgl.Map({
 var myJSON = [];
 async function loadJSON() {
   try {
-    const response = await fetch("MusicBeat.json");
+    const response = await fetch("../../MusicBeat.json");
     myJSON = await response.json();
   } catch (error) {
     console.error("Error loading JSON:", error);
@@ -75,12 +75,8 @@ const start = document.getElementById("start-game");
 const RW = document.getElementById("RW");
 const select = document.getElementById("select");
 const mapind = document.getElementById("map");
+const title = document.getElementById("title");
 mapind.style.display = "none";
-start.style.display = "none";
-RW.style.display = "none";
-select.style.display = "none";
-let lobbyId;
-popup.style.display = "none";
 
 let point = 0;
 create.addEventListener("click", () => {
@@ -94,6 +90,7 @@ function createLobby() {
   myJSON.forEach((element) => {
     stateList.push(element.State);
   });
+
   stateList = shuffleArray(stateList);
   console.log(stateList);
   const userId = document.getElementById("creator").value;
@@ -109,6 +106,7 @@ function createLobby() {
     state: "waiting",
     guess: "default",
   });
+
   onDisconnect(newLobbyRef).remove();
   const playerIDRef = ref(
     db,
@@ -143,6 +141,7 @@ join.addEventListener("click", () => {
 });
 
 function joinLobby() {
+  title.style.display = "none";
   const lobbyName = document.getElementById("lobby-id").value;
   const userId = document.getElementById("user-id").value;
   lobbyId = lobbyName;
@@ -189,6 +188,7 @@ function joinLobby() {
 }
 
 function startGame(lobbyId) {
+  title.style.display = "none";
   const lobbyRef = ref(db, "lobbies/" + lobbyId);
   update(lobbyRef, { state: "started" });
   startGameForPlayer(lobbyId);
